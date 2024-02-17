a2efd4d941e9783efd8494f290c5bc40114bdba1
Justin <alexanderrr416@gmail.com>
2024-02-17 02:22:07 -0500

    chore: organize import statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
+import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import IssueActions from "./IssueActions";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma?.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
